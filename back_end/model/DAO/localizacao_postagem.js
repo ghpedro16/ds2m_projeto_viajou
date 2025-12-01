4d052d0d422fe559497260d223c16d3feee61284
PEDRO HENRIQUE ARAUJO DA SILVA <25132515@CFP127ALUNO.LOCAL>
2025-12-01 11:17:15 -0300

    Codificando os arquivos DAO das tabelas de relacionamento

---
+++ back_end/model/DAO/localizacao_postagem.js
@@ -0,0 +1,12 @@
+/***************************************************************************************************************************************************
+* Objetivo: Arquivo responsavel pelo CRUD de dados no MySQL referente a tabela de relacionamento de localizacao com postagem do projeto Viajou!
+* Data: 01/12/2025
+* Autor: Pedro Silva
+* Versao: 1.0
+******************************************************************************************************************************************************/
+
+//Import da dependencia do Prisma que permite a execucao de Script SQL no banco de dados
+const { PrismaClient } = require('../../generated/prisma')
+
+//Criando um novo objeto baseado na classe do PrismaClient
+const prisma = new PrismaClient()

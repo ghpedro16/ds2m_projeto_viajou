c6a976b589b9500a736c3936ae4c7e58ea74ccae
GUILHERME MOREIRA DE SOUZA <25133025@cfp127aluno.local>
2025-12-02 08:40:11 -0300

    fazendo funcao de carregar as postagens no perfil

--- front_end/pages/perfil/perfil.js
+++ front_end/pages/perfil/perfil.js
@@ -1,9 +1,9 @@
 'use strict'
 
-//No futuro quando tiver usuario logado, pegar o id dele do localStorage e passar na url
-const API_URL = "http://localhost:3003/usuario"
-
 async function carregarPerfil() {
+    //No futuro quando tiver usuario logado, pegar o id dele do localStorage e passar na url
+    const API_URL = 'http://localhost:3003/usuario'
+
     try {
         const resposta = await fetch(API_URL)
         const usuarios = await resposta.json()
@@ -12,47 +12,47 @@ async function carregarPerfil() {
         criarPerfil(usuario)
 
     } catch (erro) {
-        console.error("Erro ao carregar perfil:", erro)
+        console.error('Erro ao carregar perfil:', erro)
     }
 }
 
 function criarPerfil(user) {
-    const dadosPerfil = document.querySelector(".dadosPerfil")
+    const dadosPerfil = document.querySelector('.dadosPerfil')
 
     // FOTO
-    const img = document.createElement("img")
+    const img = document.createElement('img')
     img.src = user.url_foto
 
     // DIV PARA ALINHAMENTO
-    const alinhamento = document.createElement("div")
-    alinhamento.classList.add("alinhamento")
+    const alinhamento = document.createElement('div')
+    alinhamento.classList.add('alinhamento')
 
     // NOMES
-    const nomes = document.createElement("div")
-    nomes.classList.add("nomes")
+    const nomes = document.createElement('div')
+    nomes.classList.add('nomes')
 
-    const nomeExibicao = document.createElement("span")
-    nomeExibicao.classList.add("nomeExibicao")
+    const nomeExibicao = document.createElement('span')
+    nomeExibicao.classList.add('nomeExibicao')
     nomeExibicao.textContent = user.nome
 
-    const nomeUsuario = document.createElement("span")
-    nomeUsuario.classList.add("nomeUsuario")
+    const nomeUsuario = document.createElement('span')
+    nomeUsuario.classList.add('nomeUsuario')
     nomeUsuario.textContent = user.nome_usuario
 
     nomes.appendChild(nomeExibicao)
     nomes.appendChild(nomeUsuario)
 
     // INTERAÇÕES
-    const interacoes = document.createElement("div")
-    interacoes.classList.add("interacoes")
+    const interacoes = document.createElement('div')
+    interacoes.classList.add('interacoes')
 
-    const seguidores = document.createElement("span")
+    const seguidores = document.createElement('span')
     seguidores.textContent = `Seguidores ${user.quantidade_seguidores}`
 
-    const seguindo = document.createElement("span")
+    const seguindo = document.createElement('span')
     seguindo.textContent = `Seguindo ${user.quantidade_seguindos}`
 
-    const viagens = document.createElement("span")
+    const viagens = document.createElement('span')
     viagens.textContent = `Viagens ${user.quantidade_postagens}`
 
     interacoes.appendChild(seguidores)
@@ -60,8 +60,8 @@ function criarPerfil(user) {
     interacoes.appendChild(viagens)
 
     // BIOGRAFIA
-    const biografia = document.createElement("div")
-    biografia.classList.add("biografia")
+    const biografia = document.createElement('div')
+    biografia.classList.add('biografia')
     biografia.textContent = user.biografia
 
     // Adicionando na div de alinhamento
@@ -70,11 +70,11 @@ function criarPerfil(user) {
     alinhamento.appendChild(biografia)
 
     // BOTÃO
-    const divBotao = document.createElement("div")
-    divBotao.classList.add("botoes")
+    const divBotao = document.createElement('div')
+    divBotao.classList.add('botoes')
 
-    const btnEditar = document.createElement("button")
-    btnEditar.textContent = "editar"
+    const btnEditar = document.createElement('button')
+    btnEditar.textContent = 'editar'
 
     divBotao.appendChild(btnEditar)
 
@@ -85,3 +85,36 @@ function criarPerfil(user) {
 }
 
 carregarPerfil()
+
+// async function carregarPostagem() {
+
+//     const API_URL = 'http://localhost:3003/postagem'
+
+//     try {
+//         const resposta = await fetch(API_URL)
+//         const postagens = await resposta.json()
+
+//         const postagem = postagens[0] //Provisório
+//         criarPostagem(postagem)
+//     } catch (error) {
+//         console.error("Erro ao carregar postagens:", erro)
+//     }
+// }
+
+// function criarPostagem(postagem) {
+//     const conjuntoPostagem = document.getElementById("conjuntoPostagens")
+
+//     //div postagem
+//     const postagem = document.createElement('div')
+//     postagem.classList.add('conjuntoPostagens')
+
+//     //imagem
+//     const imagem
+// }
+
+
+
+
+
+
+

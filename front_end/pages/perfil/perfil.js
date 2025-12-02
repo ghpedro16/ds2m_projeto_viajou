4f34275a94036681e80bd10f3d7f81a7ad609414
Guilherme1108 <guilherme.moreira1108@gmail.com>
2025-12-02 17:26:47 -0300

    postagens do usuario sendo pegadas com sucesso pelo json-server

--- front_end/pages/perfil/perfil.js
+++ front_end/pages/perfil/perfil.js
@@ -20,8 +20,11 @@ function criarPerfil(user) {
     const dadosPerfil = document.querySelector('.dadosPerfil')
 
     // FOTO
-    const img = document.createElement('img')
-    img.src = user.url_foto
+    const imagemPerfil = document.createElement('img')
+    imagemPerfil.src = user.url_foto
+    imagemPerfil.onerror = () => {
+        imagemPerfil.src = '../img/no_image.jpg';
+    };
 
     // DIV PARA ALINHAMENTO
     const alinhamento = document.createElement('div')
@@ -79,42 +82,97 @@ function criarPerfil(user) {
     divBotao.appendChild(btnEditar)
 
     // Adicionndo no div principal
-    dadosPerfil.appendChild(img)
+    dadosPerfil.appendChild(imagemPerfil)
     dadosPerfil.appendChild(alinhamento)
     dadosPerfil.appendChild(divBotao)
 }
 
 carregarPerfil()
 
-// async function carregarPostagem() {
+//Variavel que guarda todas as postagens
+let todasPostagens = []
 
-//     const API_URL = 'http://localhost:3003/postagem'
+// CARREGAR TODAS AS POSTAGENS
+async function carregarPostagem() {
 
-//     try {
-//         const resposta = await fetch(API_URL)
-//         const postagens = await resposta.json()
+    const API_URL = 'http://localhost:3003/postagem'
 
-//         const postagem = postagens[0] //Provisório
-//         criarPostagem(postagem)
-//     } catch (error) {
-//         console.error("Erro ao carregar postagens:", erro)
-//     }
-// }
+    try {
+        const resposta = await fetch(API_URL)
+        todasPostagens = await resposta.json()
 
-// function criarPostagem(postagem) {
-//     const conjuntoPostagem = document.getElementById("conjuntoPostagens")
+        todasPostagens.forEach(post => {
+            criarPostagem(post)
+        })
 
-//     //div postagem
-//     const postagem = document.createElement('div')
-//     postagem.classList.add('conjuntoPostagens')
-
-//     //imagem
-//     const imagem
-// }
+    } catch (erro) {
+        console.error("Erro ao carregar postagens:", erro)
+    }
+}
 
 
+// CRIAR UMA POSTAGEM
+function criarPostagem(dadosPostagem) {
+    const conjuntoPostagens = document.getElementById("conjuntoPostagens")
+
+    // DIV PRINCIPAL
+    const postagem = document.createElement('div')
+    postagem.classList.add('postagem')
+
+    //DIV da imagem mais do overlay
+    const imagemContainer = document.createElement("div")
+    imagemContainer.classList.add("imagemContainer")
+
+    // IMAGEM
+    const imagem = document.createElement('img')
+    imagem.classList.add('imagemPostagem')
+    imagem.src = dadosPostagem.midia[0]   // primeira imagem da lista
+    imagem.alt = "Imagem da postagem"
+    imagem.onerror = () => {
+        imagem.src = '../img/no_image.jpg';
+    };
+
+    // PARTE INFERIOR
+    const inferior = document.createElement('div')
+    inferior.classList.add('inferiorPostagem')
+
+    const descricao = document.createElement('p')
+    descricao.classList.add('descricao')
+    descricao.textContent = dadosPostagem.titulo
+
+    const like = document.createElement('img')
+    like.src = "../img/CoracaoVazio.svg"
+    like.classList.add('interacaoPostagem')
+
+    const chat = document.createElement('img')
+    chat.src = "../img/Chat.svg"
+    chat.classList.add('interacaoPostagem')
+
+    const favorito = document.createElement('img')
+    favorito.src = "../img/FavoritoVazio.svg"
+    favorito.classList.add('interacaoPostagem')
+
+    const overlay = document.createElement("div")
+    overlay.classList.add("overlayPostagem")
+    overlay.textContent = "Clique para ver mais"
+
+    // Adicionando
+    inferior.appendChild(descricao)
+    inferior.appendChild(like)
+    inferior.appendChild(chat)
+    inferior.appendChild(favorito)
+
+    imagemContainer.appendChild(imagem)
+    imagemContainer.appendChild(overlay)
+
+    postagem.appendChild(imagemContainer)
+    postagem.appendChild(inferior)
+
+    conjuntoPostagens.appendChild(postagem)
+}
+
+// CHAMAR FUNÇÃO
+carregarPostagem()
 
 
-
-
-
+//Editando o perfil

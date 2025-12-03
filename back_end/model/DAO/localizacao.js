c7fc3a3100c92f6ad634423076e4b4ea82485613
Guilherme1108 <guilherme.moreira1108@gmail.com>
2025-12-03 15:09:40 -0300

    arrumando comentarios da DAO da localizacao

--- back_end/model/DAO/localizacao.js
+++ back_end/model/DAO/localizacao.js
@@ -3,6 +3,9 @@
 * Data: 01/12/2025
 * Autor: Pedro Silva
 * Versao: 1.0
+
+* Autor: Guilherme Moreira
+* OBS: Corrigindo bugs da função de atualizar
 ************************************************************************************************************************************************/
 
 //Import da dependencia do Prisma que permite a execucao de Script SQL no banco de dados

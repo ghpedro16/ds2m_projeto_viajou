03673ba25d50ffe9c4ae957bff2ea71d20864cd5
Guilherme Moreira <guilherme.moreira1108@gmail.com>
2025-12-04 23:40:12 -0300

    postagens na tela de feed sendo criadas pelo js

--- front_end/pages/feed/feed.js
+++ front_end/pages/feed/feed.js
@@ -104,3 +104,104 @@ function esconderPostagens() {
 function mostrarPostagens() {
     cardsPostagens.forEach(card => card.style.display = "flex")
 }
+
+
+
+let usuarios = []
+let postagens = []
+
+async function carregarDados() {
+    try {
+        const respostaUsuarios = await fetch("http://localhost:3003/usuario")
+        usuarios = await respostaUsuarios.json()
+
+        const respostaPostagens = await fetch("http://localhost:3003/postagem")
+        postagens = await respostaPostagens.json()
+
+        montarFeed()
+
+    } catch (erro) {
+        console.error("Erro ao carregar dados:", erro)
+    }
+}
+
+function montarFeed() {
+    const container = document.getElementById("listaPostagens")
+    container.innerHTML = ""
+
+    postagens.forEach(post => {
+        const usuario = usuarios.find(usuario => Number(usuario.id) === Number(post.id_usuario))
+
+        if (!usuario) return
+
+        const elemento = criarPostagem(post, usuario)
+        container.appendChild(elemento)
+    })
+}
+
+function criarPostagem(post, user) {
+
+    const div = document.createElement("div")
+    div.classList.add("postagem")
+
+    // ======= PARTE SUPERIOR =======
+    const topo = document.createElement("div")
+    topo.classList.add("superiorPostagem")
+
+    const imgPerfil = document.createElement("img")
+    imgPerfil.src = user.url_foto
+    imgPerfil.classList.add("imagemPerfil")
+    imgPerfil.onerror = () => {
+        imgPerfil.src = '../img/no_image.jpg';
+    };
+
+    const nome = document.createElement("span")
+    nome.classList.add("nome")
+    nome.textContent = user.nome
+
+    const data = document.createElement("span")
+    data.classList.add("data")
+    data.textContent = formatarData(post.data_postagem)
+
+    topo.append(imgPerfil, nome, data)
+
+    const midia = document.createElement("img")
+    midia.src = post.midia[0]
+    midia.classList.add("imagemPostagem")
+    midia.onerror = () => {
+        midia.src = '../img/no_image.jpg';
+    };
+
+
+    // ======= PARTE INFERIOR =======
+    const inferior = document.createElement("div")
+    inferior.classList.add("inferiorPostagem")
+
+    const desc = document.createElement("p")
+    desc.classList.add("descricao")
+    desc.textContent = post.descricao
+
+    const like = document.createElement("img")
+    like.src = "../img/CoracaoVazio.svg"
+    like.classList.add("interacaoPostagem")
+
+    const chat = document.createElement("img")
+    chat.src = "../img/Chat.svg"
+    chat.classList.add("interacaoPostagem")
+
+    const save = document.createElement("img")
+    save.src = "../img/FavoritoVazio.svg"
+    save.classList.add("interacaoPostagem")
+
+    inferior.append(desc, like, chat, save)
+
+    // juntar tudo
+    div.append(topo, midia, inferior)
+    return div
+}
+
+function formatarData(data) {
+    return new Date(data).toLocaleDateString("pt-BR")
+}
+
+carregarDados()

45ff8fa97f82dc06f1e6cc24d456f637d41d12f2
GUILHERME MOREIRA DE SOUZA <25133025@cfp127aluno.local>
2025-12-15 09:42:47 -0300

    voltando para o modo anterior de chamar o login

--- front_end/app.js
+++ front_end/app.js
@@ -1,7 +1,3 @@
 'use strict'
 
-//para funcionar no github pages
-window.location.replace('/ds2m_projeto_viajou/front_end/pages/login/login.html')
-
-//Para funcionar no Live server
-//window.location.href = 'front_end/pages/login/login.html'
+window.location.href = 'front_end/pages/login/login.html'

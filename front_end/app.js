f1dba5cd87e592276b7e6d42e9e843836a4a8288
GUILHERME MOREIRA DE SOUZA <25133025@cfp127aluno.local>
2025-12-15 09:26:26 -0300

    mudando rota para abrir a tela de login no github pages

--- front_end/app.js
+++ front_end/app.js
@@ -1,3 +1,7 @@
 'use strict'
 
-window.location.href = 'front_end/pages/login/login.html'
+//para funcionar no github pages
+window.location.replace('/ds2m_projeto_viajou/front_end/pages/login/login.html')
+
+//Para funcionar no Live server
+//window.location.href = 'front_end/pages/login/login.html'
